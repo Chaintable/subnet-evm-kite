1e6d2e919c53e07d7ddddc311fad54665296f41e
lihe <he.li@debank.com>
2026-05-25 18:22:00 +0800

    fix: adapt IsBackup check to pipeline v0.0.63 leader.Manager API
    PushProcessor.IsBackup field removed in pipeline v0.0.63;
    backup status now exposed via tracer.LeaderManager.IsBackup() method.

--- core/blockchain.go
+++ core/blockchain.go
@@ -1274,7 +1274,7 @@ func (bc *BlockChain) writeBlockAndSetHead(block *types.Block, parentRoot common
 		// 先确保 pipeline tracer 不为空，然后再判断是否需要push kafka
 		// 上一个push kafka的block, 必然存在(至少有genesis block)
 		// 上一个push kafka的block比当前的head block还要新，说明有unwind回退，不需要处理, 即使是fork，等有更新的block的时候再一起push
-		if tracer.NodeXPusher != nil && !tracer.NodeXPusher.IsBackup && tracer.NodeXPusher.LastPushedBlock().BlockNumber <= block.NumberU64() {
+		if tracer.NodeXPusher != nil && (tracer.LeaderManager == nil || !tracer.LeaderManager.IsBackup()) && tracer.NodeXPusher.LastPushedBlock().BlockNumber <= block.NumberU64() {
 			lastPushBlock := tracer.NodeXPusher.LastPushedBlock()
 			_, dropBlocks, newBlocks := bc.getCommonAncestor(*lastPushBlock, ptypes.BlockContext{
 				BlockNumber: block.NumberU64(),
